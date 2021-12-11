5366cc99815372df926f5961dd6d70874049ac28
Federico Montoya <federico.montoya@mi.unc.edu.ar>
2021-12-11 11:27:37 -0300

    Template de app funcionando

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from datetime import datetime
 
 # Create your views here.
+def inicio(request):
+    dicc = {
+    "fecha" : datetime.today
+    }
+
+    plantilla = loader.get_template("AppCoder/inicio.html")
+
+    documento = plantilla.render(dicc)
+    return HttpResponse(documento)
